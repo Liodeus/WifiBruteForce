2fa8375b9e0003bc0e02779b5e2a9b8110045ed3
Liodeus <thibaultgalbourdin@live.fr>
2017-09-28 21:09:59 +0200

    add functions, displayMenu to display a menu, displaySSIDS to display the available wifi and bruteForce who at the moment only get from the user a name and a path

--- main.py
+++ main.py
@@ -5,16 +5,47 @@ import os
 from wifi import Cell
 
 
+def displayMenu():
+    print("1 - See available network ")
+    print("2 - Brute Force ")
+    print()
+    choice = input("Choice : ")
+    
+    if choice == "1":
+        print("\n\n\n")
+        displaySSIDS()
+        print("\n\n\n")
+        displayMenu()
 
-SSIDS = list(Cell.all('wlan0'))
+    if choice == "2":
+        bruteForce()
 
-for SSID in SSIDS:
-    if SSID.encrypted:
-        if SSID.encryption_type == "wpa2":
-            print(SSID.encryption_type, '  |  ', SSID.address, '  |  ',SSID.ssid)
+
+
+def displaySSIDS():
+    SSIDS = list(Cell.all('wlan0'))
+
+    for number, SSID in enumerate(SSIDS):
+        if SSID.encrypted:
+            if SSID.encryption_type == "wpa2":
+                print("{}   |  {}  |    {}".format(SSID.encryption_type, SSID.address, SSID.ssid))
+            else:
+                print("{}    |  {}  |    {}".format(SSID.encryption_type, SSID.address, SSID.ssid))
         else:
-            print(SSID.encryption_type, '   |  ', SSID.address, '  |  ',SSID.ssid)
-    else:
-        print('None   |  ',SSID.address, '  |  ',SSID.ssid)
+            print("{}   |  {}  |    {}".format(SSID.encryption_type, SSID.address, SSID.ssid))
 
-# nmcli d wifi connect HUAWEI_P9lite_01F4 password 0123456789 iface wlan0
+
+
+def bruteForce():
+    # Verify if name exist
+    wifiChoice = input("Wifi name : ")
+    # Same verify
+    dictionnaryPath = input("Dictionnary path : ")
+
+    # if both exist try to connect
+    # os.system(nmcli d wifi connect ssidName password 0123456789 iface wlan0)
+
+
+if __name__ == '__main__':
+    displayMenu()
+
