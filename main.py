b50c1b6081dcface6e479b378883c1a8826abb7b
root <root@localhost.localdomain>
2017-09-29 11:41:54 +0200

    verify the wifi name enter and the paht

--- main.py
+++ main.py
@@ -21,26 +21,44 @@ def displayMenu():
         bruteForce()
 
 
-
 def displaySSIDS():
+    print("Wait a few seconds...")
     SSIDS = list(Cell.all('wlan0'))
-
-    for number, SSID in enumerate(SSIDS):
-        if SSID.encrypted:
-            if SSID.encryption_type == "wpa2":
-                print("{}   |  {}  |    {}".format(SSID.encryption_type, SSID.address, SSID.ssid))
-            else:
-                print("{}    |  {}  |    {}".format(SSID.encryption_type, SSID.address, SSID.ssid))
-        else:
-            print("{}   |  {}  |    {}".format(SSID.encryption_type, SSID.address, SSID.ssid))
+    # for number, SSID in enumerate(SSIDS):
+    #     if SSID.encrypted:
+    #         if SSID.encryption_type == "wpa2":
+    #             print("{}   |  {}  |    {}".format(SSID.encryption_type, SSID.address, SSID.ssid))
+    #         else:
+    #             print("{}    |  {}  |    {}".format(SSID.encryption_type, SSID.address, SSID.ssid))
+    #     else:
+    #         print("{}   |  {}  |    {}".format(SSID.encryption_type, SSID.address, SSID.ssid))
 
 
 
 def bruteForce():
-    # Verify if name exist
-    wifiChoice = input("Wifi name : ")
+    print("Wait a few seconds...")
+    lstSSID = list(x.ssid for x in Cell.all('wlan0'))
+
+    flagChoice = False
+
+    while not flagChoice:
+        wifiChoice = input("Wifi name : ").strip()
+        if wifiChoice not in lstSSID:
+            print("Doesn't exist... try again...")
+        else:
+            flagChoice = True
+
     # Same verify
-    dictionnaryPath = input("Dictionnary path : ")
+    flagPath = False
+
+    while not flagPath:
+        dictionnaryPath = input("Dictionnary path : ").strip()
+        if os.path.exists(dictionnaryPath):
+            flagPath = True
+        else:
+            print("Doesn't exist... try again...")
+
+    # /usr/share/wordlists
 
     # if both exist try to connect
     # os.system(nmcli d wifi connect ssidName password 0123456789 iface wlan0)
@@ -49,3 +67,4 @@ def bruteForce():
 if __name__ == '__main__':
     displayMenu()
 
+# https://stackoverflow.com/questions/21662351/connect-wifi-with-python-or-linux-terminal
